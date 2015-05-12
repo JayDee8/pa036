d8a6a46ede753772c76720e82c45e4195171953b
JayDee8 <jirka.dockal.8@seznam.cz>
2015-05-12 11:20:56 +0200

    dfsa

--- Models/AkcieDetail.cs
+++ Models/AkcieDetail.cs
@@ -23,8 +23,22 @@ namespace bcpp.Models
 
     public class SledovaneMy
     {
-        public IEnumerable<Object> sled { get; set; }
+        public IEnumerable<MyModelSledovane> sled { get; set; }
         public List<int> akcieIds { get; set; }
         public List<string> akcieNames { get; set; }
+    }
+
+    public class MyModelSledovane
+    {
+        public int idSledovane { get; set; }
+        public string nazevSledovane { get; set; }
+        public string nazevAkcie { get; set; }
+        public string nazevFirmy { get; set; }
+        [DisplayFormat(DataFormatString = "{0:0.00}", ApplyFormatInEditMode = true)]
+        public float cenaProdej { get; set; }
+        [DisplayFormat(DataFormatString = "{0:0.00}", ApplyFormatInEditMode = true)]
+        public float cenaNakup { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime datum { get; set; }
     }
 }
